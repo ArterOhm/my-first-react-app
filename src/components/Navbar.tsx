bf8e94679eb73742568f8617c5abf71fb46f163a
ArterOhm <aef117722@gmail.com>
2023-09-29 15:41:44 +0700

    navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import classes from './Navbar.module.css'
+
+const Navbar = () => {
+  return (
+    <div className={classes.navbar}>
+      <h4>Navbar</h4>
+      <button>Login</button>
+    </div>
+  )
+}
+export default Navbar
