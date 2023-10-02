154729d95e6865232281e78f373fd76824a2d76e
ArterOhm <aef117722@gmail.com>
2023-10-02 11:36:30 +0700

    demo condition rendering

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import Greeting from './components/Greeting'
 import Navbar from './components/Navbar'
 
 function App() {
+  const islogin = true
   return (
     <div className="App">
       <Navbar />
-      <Greeting name="F" age={30} />
-      <Greeting name="A" age={25} />
-      <Greeting name="B" age={27} />
+      <Greeting name="F" age={30} isLoggedIn={islogin} />
+      <Greeting name="A" age={25} isLoggedIn={islogin} />
+      <Greeting name="B" age={27} isLoggedIn={islogin} />
     </div>
   )
 }
--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -3,16 +3,21 @@ import classes from './Greeting.module.css'
 interface IGreetingProps {
   name: string
   age: number
+  isLoggedIn: boolean
 }
 
-const Greeting = ({ name, age }: IGreetingProps) => {
+const Greeting = ({ name, age, isLoggedIn }: IGreetingProps) => {
   return (
     <div className={classes.card}>
       <h3>Welcome!</h3>
-      <div>
-        <p>name:{name}</p>
-        <p>age:{age}</p>
-      </div>
+      {isLoggedIn ? (
+        <div>
+          <p>name:{name}</p>
+          <p>age:{age}</p>
+        </div>
+      ) : (
+        <p>Unknown</p>
+      )}
     </div>
   )
 }
